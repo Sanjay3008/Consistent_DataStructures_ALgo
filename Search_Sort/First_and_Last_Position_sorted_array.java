a23bc0460ed2de81338a1d5efab10bc78dbe8c32
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2022-05-10 12:21:07 +0530

    Create First_and_Last_Position_sorted_array.java

---
+++ Search_Sort/First_and_Last_Position_sorted_array.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    // Driver program to test printPowerSet
+    public static void main (String[] args)
+    {
+        int n=9, x=5;
+        int arr[] = { 1, 3, 5, 5, 5, 5, 67, 123, 125 };
+
+        boolean found = false;
+        int start_ind = 0, end_ind = 0;
+
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==x)
+            {
+                start_ind = i;
+                while(arr[i]==x)
+                {
+                    i++;
+                }
+                end_ind = i-1;
+            }
+        }
+
+        System.out.print(start_ind + " " + end_ind);
+    }
+
+}
