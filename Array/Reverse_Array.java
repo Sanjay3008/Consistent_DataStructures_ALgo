b27fc6704c607486d6540298d949f0cbed9afc93
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2022-05-10 11:47:34 +0530

    Update Reverse_Array.java

--- Array/Reverse_Array.java
+++ Array/Reverse_Array.java
@@ -11,13 +11,15 @@ public class Main {
        int array[] = {1,2,3,4,5};
 
        int i=0;
-       int j= array.length;
+       int j= array.length-1;
 
        while(i<j)
        {
            int temp = array[i];
            array[i] =  array[j];
            array[j] = temp;
+           i++;
+           j--;
        }
 
        System.out.println(Arrays.toString(array));
