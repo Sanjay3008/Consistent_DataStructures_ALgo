f1b89966736ebd5ce748e1c2eae3132c9682c951
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2022-05-10 11:44:52 +0530

    Create Reverse_Array.java

---
+++ Array/Reverse_Array.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    // Driver program to test printPowerSet
+    public static void main (String[] args)
+    {
+       int array[] = {1,2,3,4,5};
+
+       int i=0;
+       int j= array.length;
+
+       while(i<j)
+       {
+           int temp = array[i];
+           array[i] =  array[j];
+           array[j] = temp;
+       }
+
+       System.out.println(Arrays.toString(array));
+    }
+
+
+
+
+}
